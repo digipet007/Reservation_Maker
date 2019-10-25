418769c9248a9680ece26080e2cd1ebeccc7dfcc
Sarah Arnold <digipet007@gmail.com>
2019-10-24 19:23:51 -0700

    added api routes and completed ajax calls

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -3,14 +3,19 @@ var waitingListData = require('../data/waitinglist-data');
 
 //the parameter "app" represents Express
 module.exports = function (app) {
+    //WHEN the request is made, these are directions to follow
     //get function displays json object tableData whenever at the url, /api/tables
+    //...if the app tries to post to this url, run this function
     app.get('/api/tables', function (req, res) {
-        res.json(tableData);  //how does it know what table data is??
+        //parameter is variable created above
+        res.json(tableData);  
     })
+    //if the app tries to get from this url, run this function
     app.get('/api/waitlist', function (req, res) {
-        res.json(waitingListData);  //how does it know what table data is??
+        res.json(waitingListData);  
     })
     //post route will allow users make posts to api to create new reservations
+    //response is marked true or false based on what array the information is pushed into
     app.post('/api/tables', function (req, res) {
         if(tableData.length < 5) {
             tableData.push(req.body);
@@ -20,6 +25,16 @@ module.exports = function (app) {
             waitingListData.push(req.body);
             res.json(false);
         }
+    })
+    
+    //if the app tries to post to this url, run this function
+    //clears data from waitlist and reservation list
+    app.post('/api/clear', function(){
+        tableData = [];
+        waitingListData = [];
+
+        console.log(tableData);
+        console.log(waitingListData);
     })
 }
 
